b0d9ab0a2302cf6e085f0bafd2ecf8b12fe2c7d8
Nicholas Jones <njonesgrad@gmail.com>
2024-11-20 21:47:54 +0100

    Editor Page MVP
    Editor page MVP complete. All the required fields and calculations are present.

--- src/app/components/invoice-editor/invoice-editor-table/invoice-editor-table.component.ts
+++ src/app/components/invoice-editor/invoice-editor-table/invoice-editor-table.component.ts
@@ -1,28 +1,32 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { TableModule } from "primeng/table";
 import { CommonModule } from "@angular/common";
 import { ButtonModule } from "primeng/button";
+import { FormsModule } from "@angular/forms";
+import { v4 as uuidv4 } from "uuid";
 
 interface tableUserInputs {
+  rowId: string;
   name: string;
-  quantity: number | null;
-  quantUnit: number | null;
-  unitNetPrice: number | null;
-  vatPercentage: number | null;
-  totalNet: number | null;
-  totalGross: number | null;
+  quantity: string;
+  quantUnit: string;
+  unitNetPrice: string;
+  vatPercentage: string;
+  totalNet: string;
+  totalGross: string;
 }
 
 @Component({
   selector: "app-invoice-editor-table",
   standalone: true,
-  imports: [TableModule, CommonModule, ButtonModule],
+  imports: [TableModule, CommonModule, ButtonModule, FormsModule],
   templateUrl: "./invoice-editor-table.component.html",
   styleUrls: ["./invoice-editor-table.component.scss"],
 })
 export class InvoiceEditorTableComponent {
   @Input() userInvoiceEntries?: tableUserInputs[];
-  tableRows : tableUserInputs[] = [];
+  @Output()formChanges = new EventEmitter();
+  tableRows: tableUserInputs[] = []; //Row data will be inserted upon saving the changes
 
   columns = [
     { field: "name", header: "Name", class: "w-40" },
@@ -32,20 +36,60 @@ export class InvoiceEditorTableComponent {
     { field: "vat", header: "VAT %" },
     { field: "totalNet", header: "Total Net" },
     { field: "totalGross", header: "Total Gross" },
-    {field: "", header: ""}
+    { field: "", header: "" },
   ];
 
-  addTableRow():void{
-    this.tableRows.push({
-      name: "",
-      quantity: null,
-      quantUnit: null,
-      unitNetPrice: null,
-      vatPercentage: null,
-      totalNet: null,
-      totalGross: null,
-    })
+  addTableRow(): void {
+    const rowId = uuidv4();
 
-    console.log("tableRows now, ", this.tableRows);
+    this.tableRows.push({
+      rowId: rowId,
+      name: "",
+      quantity: "",
+      quantUnit: "",
+      unitNetPrice: "",
+      vatPercentage: "",
+      totalNet: "",
+      totalGross: "",
+    });
   }
+  
+  updateCalculations(rowId: string): void {
+    const tableRow = document.getElementById(`${rowId}`);
+    const tableRowArrayObject = this.tableRows.find((obj)=> obj.rowId === rowId)
+
+    if(tableRowArrayObject && tableRow){
+      //For the row with the matching ID, we will use these values to calculate our fields
+      const quantity = parseFloat(tableRowArrayObject.quantity as string);
+      const unitNetPrice = parseFloat(tableRowArrayObject.unitNetPrice as string);
+      const vatPercentage = parseFloat(tableRowArrayObject.vatPercentage as string);
+      //Input values we want to malipulate
+      const totalNetHTML= tableRow?.querySelector('td input[name="totalNet"]') as HTMLInputElement;
+      const totalGrossHTML =tableRow?.querySelector('td input[name="totalGross"]') as HTMLInputElement;
+      //Returns calculations into cells as values
+      const totalNetCalculated = (quantity * unitNetPrice);
+      const totalGrossCalculated = (quantity * (unitNetPrice + (unitNetPrice * (vatPercentage / 1000))));
+
+      totalNetHTML.value = totalNetCalculated ? totalNetCalculated.toString() : "";
+      totalGrossHTML.value = totalGrossCalculated 
+        ? totalGrossCalculated.toString() 
+        : totalNetCalculated 
+        ? totalNetCalculated.toString() 
+        : "";
+      //Set the object reference values
+      tableRowArrayObject.totalNet = totalNetHTML.value;
+      tableRowArrayObject.totalGross = totalGrossHTML.value;
+      //We want to signal to the parent component that totals need to be re-calcualted, and supply the tableRows with data
+      this.formChanges.emit(this.tableRows);
+    }
+  }
+
+  deleteRow(id : string) : void{
+    this.tableRows = this.tableRows.filter((row)=>{
+      return row.rowId !== id;
+    })
+    //We deleted a row, so we naturally want to recalculate our invoice totals in the invoice-editor component
+    this.formChanges.emit(this.tableRows);
+  }
+
 }
--- src/app/components/invoice-editor/invoice-editor.component.ts
+++ src/app/components/invoice-editor/invoice-editor.component.ts
@@ -1,34 +1,79 @@
-import { Component } from '@angular/core';
-import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { InvoiceEditorTableComponent } from './invoice-editor-table/invoice-editor-table.component';
-import { CalendarModule } from 'primeng/calendar';
+import { Component } from "@angular/core";
+import { InputTextModule } from "primeng/inputtext";
+import { FormsModule } from "@angular/forms"; // Import FormsModule
+import { InvoiceEditorTableComponent } from "./invoice-editor-table/invoice-editor-table.component";
+import { CalendarModule } from "primeng/calendar";
+import { TableModule } from "primeng/table";
 
+interface ITableUserInputs {
+  rowId: string;
+  name: string;
+  quantity: string;
+  quantUnit: string;
+  unitNetPrice: string;
+  vatPercentage: string;
+  totalNet: string;
+  totalGross: string;
+}
 
 @Component({
-  selector: 'app-invoice-editor',
+  selector: "app-invoice-editor",
   standalone: true,
-  imports: [InputTextModule, FormsModule, InvoiceEditorTableComponent, CalendarModule],
-  templateUrl: './invoice-editor.component.html',
-  styleUrl: './invoice-editor.component.scss'
+  imports: [
+    InputTextModule,
+    FormsModule,
+    InvoiceEditorTableComponent,
+    CalendarModule,
+    TableModule
+  ],
+  templateUrl: "./invoice-editor.component.html",
+  styleUrl: "./invoice-editor.component.scss",
 })
-
 export class InvoiceEditorComponent {
-  invoiceNo: string = '';
-  invoiceDate: Date | undefined;
-  date2: Date | undefined;
-  seller: string = '';
-  vatId: string = '';
-  streetNo: string = '';
-  postCode: string = '';
-  city: string = '';
-  bankAccount: string = '';
-  bank: string = '';
-  swift: string = '';
-  buyer: string = '';
-  buyerVAT: string = '';
-  buyerStreetNo: string = '';
-  buyerPostCode: string = '';
-  buyerCity: string = '';
+  public tableData: ITableUserInputs[] = [];
 
+  public invoiceData = {
+    invoiceNo: "",
+    invoiceDate: undefined as Date | undefined,
+    currency: "PLN",
+    seller: "",
+    vatId: "",
+    streetNo: "",
+    postCode: "",
+    city: "",
+    bankAccount: "",
+    bank: "",
+    swift: "",
+    buyer: "",
+    buyerVAT: "",
+    buyerStreetNo: "",
+    buyerPostCode: "",
+    buyerCity: "",
+    tableData: {},
+    netTotal: "",
+    vatTotal: "",
+    grossTotal: "",
+  };
+
+  reCalculateTotals(tableRows: ITableUserInputs[]) {
+    this.invoiceData.tableData = tableRows;
+
+    this.invoiceData.netTotal = calculateTotal("totalNet");
+    this.invoiceData.vatTotal = calculateTotal("vatPercentage");
+    this.invoiceData.grossTotal = calculateTotal("totalGross");
+
+    function calculateTotal(property: keyof (typeof tableRows)[0]): string {
+      let accumulator = 0;
+
+      tableRows.forEach((row) => {
+        if (row[property]) {
+          accumulator += parseFloat(row[property]);
+        }
+      });
+
+      return accumulator ? accumulator.toString() : '';
+    }
+
+    console.log(this.invoiceData);
+  }
 }
