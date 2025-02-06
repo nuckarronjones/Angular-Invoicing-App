d0b661a9e824aa8e60c2a7c896b5148472041ce8
Nicholas Jones <njonesgrad@gmail.com>
2025-02-06 20:50:47 +0100

    Create New Service for Editor Navbar

--- src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
+++ src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component, Output, EventEmitter } from "@angular/core";
 import { MenubarModule } from "primeng/menubar";
-import { MenuItem } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { InvoiceEditModeState } from "../../../services/toggle-edit-mode.service";
+import { EditorToolbarService } from "../../../services/editor-toolbar-service";
 import { AsyncPipe } from "@angular/common";
 
 @Component({
@@ -15,66 +15,13 @@ import { AsyncPipe } from "@angular/common";
 export class EditorNavbarComponent {
   @Output() printEvent = new EventEmitter();
 
-  items: MenuItem[] = [];
+  constructor(
+    public invoiceEditModeState: InvoiceEditModeState,
+    private _editorToolbarService: EditorToolbarService
+  ) {}
 
-  constructor(public invoiceEditModeState: InvoiceEditModeState) {}
-
-  printInvoice(): void {
-    const printContent = document.getElementById('printable-area');
-    if (printContent) {
-      const styles = Array.from(document.querySelectorAll('style, link[rel="stylesheet"]'))
-        .map(style => style.outerHTML)
-        .join('\n');
-
-      const content = `
-        <html>
-          <head>
-            <title>Print Preview</title>
-            ${styles}
-            <style>
-              @page { size: auto;  margin: 0mm; }
-
-              *, input {
-                font-size: 12px !important;
-                color: black;
-              }
-              body {
-                padding: 15px;
-              }
-              .input-value {
-                line-height: 4px !important;
-                margin-bottom: 20px;
-              }
-              .logo-image{
-                width:150px !important;
-                height:150px !important;
-              }
-            </style>
-          </head>
-          <body>
-            <div id="printable-area" style="padding:5mm;">
-              ${printContent.innerHTML}
-            </div>
-          </body>
-        </html>
-      `;
-  
-      const printWindow = window.open();
-      if (printWindow) {
-        printWindow.document.open();
-        printWindow.document.write(content);
-        printWindow.document.close();
-  
-        printWindow.onload = () => {
-          printWindow.print();
-          printWindow.close();
-        };
-      } else {
-        console.error('Failed to open print window');
-      }
-    } else {
-      console.error('Printable area not found');
-    }
+  public printInvoice(){
+    this._editorToolbarService.printInvoice();
   }
   
 }
---
+++ src/app/services/editor-toolbar-service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditorToolbarService {
+  
+    printInvoice(): void {
+        const printContent = document.getElementById('printable-area');
+        if (printContent) {
+          const styles = Array.from(document.querySelectorAll('style, link[rel="stylesheet"]'))
+            .map(style => style.outerHTML)
+            .join('\n');
+    
+          const content = `
+            <html>
+              <head>
+                <title>Print Preview</title>
+                ${styles}
+                <style>
+                  @page { size: auto;  margin: 0mm; }
+    
+                  *, input {
+                    font-size: 12px !important;
+                    color: black;
+                  }
+                  body {
+                    padding: 15px;
+                  }
+                  .input-value {
+                    line-height: 4px !important;
+                    margin-bottom: 20px;
+                  }
+                  .logo-image{
+                    width:150px !important;
+                    height:150px !important;
+                  }
+                </style>
+              </head>
+              <body>
+                <div id="printable-area" style="padding:5mm;">
+                  ${printContent.innerHTML}
+                </div>
+              </body>
+            </html>
+          `;
+      
+          const printWindow = window.open();
+          if (printWindow) {
+            printWindow.document.open();
+            printWindow.document.write(content);
+            printWindow.document.close();
+      
+            printWindow.onload = () => {
+              printWindow.print();
+              printWindow.close();
+            };
+          } else {
+            console.error('Failed to open print window');
+          }
+        } else {
+          console.error('Printable area not found');
+        }
+      }
+
+}
