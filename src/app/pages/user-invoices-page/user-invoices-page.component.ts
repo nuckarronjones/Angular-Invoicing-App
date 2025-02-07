074c70339adf9b20010fa17f1b4be85be6d9a144
Nicholas Jones <njonesgrad@gmail.com>
2025-02-07 21:38:16 +0100

    User Invoices Component
    Responsible for showing a table of all saved user invoices. Users can then edit, view existing invoices - or create new ones (these features will come soon).

--- src/app/pages/user-invoices-page/user-invoices-page.component.ts
+++ src/app/pages/user-invoices-page/user-invoices-page.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserInvoicesService } from "../../services/api/user-invoices.service";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
+import { MenuModule } from "primeng/menu";
+import { FormsModule } from "@angular/forms";
+import { NgIf } from "@angular/common";
 
 @Component({
-  selector: 'app-user-invoices-page',
+  selector: "app-user-invoices-page",
   standalone: true,
-  imports: [],
-  templateUrl: './user-invoices-page.component.html',
-  styleUrl: './user-invoices-page.component.scss'
+  imports: [
+    ProgressSpinnerModule,
+    NgIf,
+    TableModule,
+    ButtonModule,
+    MenuModule,
+    FormsModule,
+  ],
+  templateUrl: "./user-invoices-page.component.html",
+  styleUrl: "./user-invoices-page.component.scss",
 })
-export class UserInvoicesPageComponent {
+export class UserInvoicesPageComponent implements OnInit {
+  constructor(public userInvoicesService: UserInvoicesService) {}
 
+  public isLoading: boolean = true;
+  public allUserInvoices: any[] | null = null;
+  public actionItems: any[] = [];
+
+  selectedCity: any;
+
+  ngOnInit(): void {
+    this.userInvoicesService.getUserInvoices();
+
+    this.userInvoicesService.userInvoices$.subscribe((value) => {
+      if (value !== null) {
+        this.isLoading = false;
+        this.allUserInvoices = value;
+      }
+    });
+
+    this.actionItems = [
+      { label: "Edit", icon: "pi pi-pencil" },
+      { label: "View", icon: "pi pi-search" },
+    ];
+  }
 }
