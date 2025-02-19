cab7755aa8749c5f68e669f9081c08d888bcb3e2
Nicholas Jones <njonesgrad@gmail.com>
2025-02-19 21:50:34 +0100

    Handle Invoice Data Passed into Editor (Fix New Invoice Creation)
    Instead of rendering an id (if we dont already have one) before passing into the editor component, we instead generate an ID within the component. This fixes the issue where we can not save away new invoices that were not retrieved from already existing stored invoices - as the generated ID kept changing as we passed it in.

--- src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
+++ src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component, Output, EventEmitter, Input } from "@angular/core";
+import { Component, Output, EventEmitter, Input, OnInit } from "@angular/core";
 import { MenubarModule } from "primeng/menubar";
 import { ButtonModule } from "primeng/button";
 import { InvoiceEditModeState } from "../../../services/toggle-edit-mode.service";
 import { EditorToolbarService } from "../../../services/editor-toolbar-service";
 import { AsyncPipe } from "@angular/common";
+import { documentData } from "../../../models/document-data.model";
+import { generateNewInvoiceId } from "../../../functions/generate-invoice-id";
 
 @Component({
   selector: "app-editor-navbar",
@@ -12,23 +14,40 @@ import { AsyncPipe } from "@angular/common";
   templateUrl: "./editor-navbar.component.html",
   styleUrl: "./editor-navbar.component.scss",
 })
-export class EditorNavbarComponent {
-  @Input() invoiceId: string = "";
+export class EditorNavbarComponent implements OnInit {
   @Input() currentInvoice?: any = null;
-
   @Output() printEvent = new EventEmitter();
+
+  public newInvoiceID: string = "";
 
   constructor(
     public invoiceEditModeState: InvoiceEditModeState,
     private _editorToolbarService: EditorToolbarService
   ) {}
 
-  public printInvoice(){
+  ngOnInit(): void {
+    if (!this.currentInvoice) {
+      this.currentInvoice = this._createBlankDocument();
+      this.newInvoiceID = generateNewInvoiceId();
+      this.currentInvoice.id = this.newInvoiceID;
+    }
+  }
+
+  public printInvoice() {
     this._editorToolbarService.printInvoice();
   }
 
-  public saveInvoice(){
-    this._editorToolbarService.saveInvoice(this.invoiceId, this.currentInvoice);
+  public saveInvoice() {
+    const invoiceId =
+      this.currentInvoice.invoiceId
+        ? this.currentInvoice.invoiceId
+        : this.newInvoiceID;
+
+    this._editorToolbarService.saveInvoice(invoiceId, this.currentInvoice);
+  }
+
+  private _createBlankDocument() {
+    return documentData;
   }
   
 }
