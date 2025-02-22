0f758adf56c581b3ade60c427a6dc69c44e4e0ff
Nicholas Jones <njonesgrad@gmail.com>
2025-02-22 11:20:51 +0100

    Implement Success Message on Save

--- src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
+++ src/app/pages/invoice-editor-page/editor-navbar/editor-navbar.component.ts
@@ -3,20 +3,31 @@ import { MenubarModule } from "primeng/menubar";
 import { ButtonModule } from "primeng/button";
 import { InvoiceEditModeState } from "../../../services/toggle-edit-mode.service";
 import { EditorToolbarService } from "../../../services/editor-toolbar-service";
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, NgIf } from "@angular/common";
 import { documentData } from "../../../models/document-data.model";
 import { UserInvoicesServiceApi } from "../../../services/api/user-invoices.service";
 import { DocumentData } from "../../../enums/invoice-document.enum";
+import { Message } from "primeng/api";
+import { MessagesModule } from "primeng/messages";
+import { RippleModule } from "primeng/ripple";
 
 @Component({
   selector: "app-editor-navbar",
   standalone: true,
-  imports: [MenubarModule, ButtonModule, AsyncPipe],
+  imports: [
+    MenubarModule,
+    ButtonModule,
+    AsyncPipe,
+    MessagesModule,
+    RippleModule,
+    NgIf,
+  ],
   templateUrl: "./editor-navbar.component.html",
   styleUrl: "./editor-navbar.component.scss",
 })
 export class EditorNavbarComponent implements OnInit {
-  private currentInvoice: DocumentData = documentData;
+  public messages: Message[] | undefined;
+  private _currentInvoice: DocumentData = documentData;
 
   @Output() printEvent = new EventEmitter();
 
@@ -29,7 +40,7 @@ export class EditorNavbarComponent implements OnInit {
   ngOnInit(): void {
     this._userInvoicesServiceApi.currentInvoice$.subscribe((invoice) => {
       if (invoice) {
-        this.currentInvoice = invoice;
+        this._currentInvoice = invoice;
       }
     });
   }
@@ -39,7 +50,20 @@ export class EditorNavbarComponent implements OnInit {
   }
 
   public saveInvoice(): void {
-    this._editorToolbarService.saveInvoice(this.currentInvoice.id, this.currentInvoice);
+    this._editorToolbarService.saveInvoice(
+      this._currentInvoice.id,
+      this._currentInvoice
+    );
+    this._renderStatusMessage();
   }
 
+  private _renderStatusMessage(): void {
+    this.messages = [
+      { severity: "success", summary: "Invoice Saved" },
+    ];
+
+    setTimeout(() => {
+      this.messages = [];
+    }, 1800);
+  }
 }
