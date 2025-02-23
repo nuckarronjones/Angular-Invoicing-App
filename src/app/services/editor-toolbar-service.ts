7347de454ccfec9fe07b1be35b6c8ae6e9bc3e15
Nicholas Jones <njonesgrad@gmail.com>
2025-02-23 16:36:48 +0100

    Fix Print View
    Fix print view, and most importantly, import header style tags to retain the same styling for the component we are trying to print (printable-area). Also, found out to add <Script> to simply print the window onLoad, as we will have everything we need onLoad.

--- src/app/services/editor-toolbar-service.ts
+++ src/app/services/editor-toolbar-service.ts
@@ -3,55 +3,65 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class EditorToolbarService {
 
+export class EditorToolbarService {
   public saveInvoice(invoiceId: string, currentInvoice: any): void {
     localStorage.setItem(invoiceId, JSON.stringify(currentInvoice));
   }
 
-  
   public printInvoice(): void {
-    const invoiceContent = document.getElementById("printable-area")?.innerHTML;
+    const printContent = document.getElementById("printable-area");
 
-    if (invoiceContent) {
-      const content = this._formatPreviewContent(invoiceContent);
-      this._generatePrintWindow(content);
-    } else {
-      console.error("Printable area not found");
+    if (printContent) {
+      const newPrintingTab = window.open("  ", "_blank");
+
+      if (newPrintingTab) {
+        const formattedPrintContent = this._formatPreviewContent(printContent);
+
+        newPrintingTab.document.write(formattedPrintContent);
+        newPrintingTab.document.close();
+      }
     }
   }
 
-  private _formatPreviewContent(invoiceContent: string): string {
-
+  private _formatPreviewContent(invoiceContent: HTMLElement): string {
     return `
-    <html>
-      <head>
-        <title>Print Preview</title>
-        <link rel="stylesheet" href="/print-styles.css" />
-        <style> </style>
-      </head>
-      <body>
-        <div id="printable-area" style="padding:5mm;">
-          ${invoiceContent}
-        </div>
-      </body>
-    </html>
-  `;
+          <html>
+          <head>
+            <title>Print Preview</title>
+            ${document.head.innerHTML}
+            <style>
+             ${this._printWindowStyling}
+            </style>
+          </head>
+          <body>
+            ${invoiceContent.outerHTML}
+            <script>
+              window.onload = function() {
+                window.print();
+              };
+            </script>
+          </body>
+          </html>
+        `;
   }
 
-  private _generatePrintWindow(content: string): void {
-    const printWindow = window.open();
-
-    if (printWindow) {
-      printWindow.document.open();
-      printWindow.document.write(content);
-      printWindow.document.close();
-
-      printWindow.onload = () => {
-        printWindow.print();
-      };
-    } else {
-      console.error("Failed to open print window");
-    }
+  private get _printWindowStyling(): string {
+    return `
+       @page{
+          size: auto;
+          margin: 0;
+        }
+        body {
+          margin: 0;
+          padding: 0;
+        }
+        #printable-area{
+          zoom: .9 !important;
+          overflow:hidden;
+          height: auto !important;
+          box-shadow: none !important;
+        }
+    `;
   }
 }
