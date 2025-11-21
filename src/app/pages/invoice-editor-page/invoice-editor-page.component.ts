353a50a7e170959fc88670030b67d3549984f640
Nicholas Jones <njonesgrad@gmail.com>
2025-11-21 12:48:16 +0100

    Finish Initialisation of Blank Invoice Form
    Considering I am refactoring this app, I will keep another set of invoice configs / interfaces inside this component for now. Will not be referencing form-fields.model.ts.

--- src/app/pages/invoice-editor-page/invoice-editor-page.component.ts
+++ src/app/pages/invoice-editor-page/invoice-editor-page.component.ts
@@ -7,16 +7,176 @@ import { CommonModule } from "@angular/common";
 import { AsyncPipe } from "@angular/common";
 import { NgIf } from "@angular/common";
 import { FormArray, FormControl, FormGroup } from "@angular/forms";
-import { formFields } from "../../config/form-fields.model";
-
-interface InvoiceFormGroup {
-  header: FormArray<FormGroup<InputField>>;
-  headerImage: FormControl;
-  body: FormArray<FormGroup<InputField>>;
-  footer: FormArray<FormGroup<InputField>>;
-}
 
 interface InputField {
+  id: string;
+  label: string;
+  placeholder: string;
+  inputType: "date" | "text";
+  style: "w-100" | "w-50" | "w-50 inline-block";
+  column: "left" | "right";
+}
+
+interface Formfields {
+  header: ReadonlyArray<InputField>;
+  headerImage: string;
+  body: ReadonlyArray<InputField>;
+  footer: ReadonlyArray<InputField>;
+}
+
+const invoiceConfig: Formfields = {
+  headerImage: "",
+  header: [
+    {
+      id: "invoiceNo",
+      label: "Invoice No:",
+      placeholder: "Enter Invoice No",
+      inputType: "text",
+      style: "w-50",
+      column: "left",
+    },
+    {
+      id: "invoiceDate",
+      label: "Invoice Date:",
+      placeholder: "",
+      inputType: "date",
+      style: "w-50",
+      column: "left",
+    },
+    {
+      id: "invoiceDueDate",
+      label: "Invoice Due Date:",
+      placeholder: "",
+      inputType: "date",
+      style: "w-50",
+      column: "left",
+    },
+    {
+      id: "invoiceType",
+      label: "Invoice Type:",
+      placeholder: "Enter Invoice Type",
+      inputType: "text",
+      style: "w-50",
+      column: "left",
+    },
+  ],
+  body: [
+    {
+      id: "seller",
+      label: "Seller:",
+      placeholder: "Enter Seller Information",
+      inputType: "text",
+      style: "w-100",
+      column: "left",
+    },
+    {
+      id: "sellerVAT",
+      label: "VAT ID:",
+      placeholder: "Enter VAT",
+      inputType: "text",
+      style: "w-100",
+      column: "left",
+    },
+    {
+      id: "sellerStreetNo",
+      label: "Street and No:",
+      placeholder: "Enter Street and No",
+      inputType: "text",
+      style: "w-100",
+      column: "left",
+    },
+    {
+      id: "sellerPostCode",
+      label: "Post Code:",
+      placeholder: "Enter Post Code",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "left",
+    },
+    {
+      id: "sellerCity",
+      label: "City:",
+      placeholder: "Enter City",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "left",
+    },
+    {
+      id: "sellerBankAcct",
+      label: "Bank Account:",
+      placeholder: "Enter Bank Account",
+      inputType: "text",
+      style: "w-100",
+      column: "left",
+    },
+    {
+      id: "sellerBank",
+      label: "Bank:",
+      placeholder: "Enter Bank",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "left",
+    },
+    {
+      id: "sellerSwift",
+      label: "Swift:",
+      placeholder: "Enter Swift",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "left",
+    },
+    {
+      id: "buyer",
+      label: "Buyer:",
+      placeholder: "Enter Buyer Information",
+      inputType: "text",
+      style: "w-100",
+      column: "right",
+    },
+    {
+      id: "buyerVAT",
+      label: "Buyer VAT ID:",
+      placeholder: "Enter Buyer VAT",
+      inputType: "text",
+      style: "w-100",
+      column: "right",
+    },
+    {
+      id: "buyerStreetNo",
+      label: "Buyer Street and No:",
+      placeholder: "Enter Buyer Street and No",
+      inputType: "text",
+      style: "w-100",
+      column: "right",
+    },
+    {
+      id: "buyerPostCode",
+      label: "Post Code:",
+      placeholder: "Enter Post Code",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "right",
+    },
+    {
+      id: "buyerCity",
+      label: "City:",
+      placeholder: "Enter City",
+      inputType: "text",
+      style: "w-50 inline-block",
+      column: "right",
+    },
+  ],
+  footer: [],
+};
+
+interface InvoiceFormGroup {
+  header: FormArray<FormGroup<FormInputField>>;
+  headerImage: FormControl;
+  body: FormArray<FormGroup<FormInputField>>;
+  footer: FormArray<FormGroup<FormInputField>>;
+}
+
+interface FormInputField {
   id: FormControl<string | null>;
   value: FormControl<string | null>;
   label: FormControl<string>;
@@ -53,12 +213,11 @@ export class InvoiceEditorPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // See if we are on /invoices or /invoice/:id
     this._createInvoiceFormGroup();
   }
 
   private _createInvoiceFormGroup(): void {
-    Object.keys(formFields).forEach((section) => {
+    Object.keys(invoiceConfig).forEach((section) => {
       if (section !== "headerImage") {
         this._createInputFields(section);
       }
@@ -66,13 +225,26 @@ export class InvoiceEditorPageComponent implements OnInit {
   }
 
   private _createInputFields(key: string): void {
-    const keyValue = `${key}` as keyof InvoiceFormGroup;
+    // Convert key string into what key for what object
+    const invoiceFormKey = `${key}` as keyof InvoiceFormGroup;
+    const invoiceConfigKey = `${key}` as keyof Formfields;
 
-    const invoiceFormGroupFormArray = this.invoiceFormGroup.get(keyValue) as FormArray;
+    const invoiceFormGroupFormArray = this.invoiceFormGroup.get(invoiceFormKey) as FormArray;
 
-    formFields[keyValue].forEach(()=>{
-      
-    })
+    (invoiceConfig[invoiceConfigKey] as InputField[]).forEach(inputField => {
+      invoiceFormGroupFormArray.push(
+        new FormGroup({
+          id: new  FormControl(inputField.id),
+          value: new FormControl(null),
+          label:new FormControl(inputField.label),
+          placeholder:new FormControl(inputField.placeholder),
+          inputType: new FormControl(inputField.inputType),
+          style: new FormControl(inputField.style),
+          column: new FormControl(inputField.column)
+        })
+      )
+    });
+
   }
 
   // Create new invoice form on /invoices route
