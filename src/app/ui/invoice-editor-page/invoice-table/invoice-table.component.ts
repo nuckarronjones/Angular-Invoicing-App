bbd76d7879ab7911d6ded777414ba7696e1bac17
Nicholas Jones <njonesgrad@gmail.com>
2025-12-06 21:29:43 +0100

    Cleanup component - unused functions and comments

--- src/app/ui/invoice-editor-page/invoice-table/invoice-table.component.ts
+++ src/app/ui/invoice-editor-page/invoice-table/invoice-table.component.ts
@@ -1,9 +1,6 @@
 import {
   Component,
   Input,
-  // Output,
-  // EventEmitter,
-  OnInit,
 } from "@angular/core";
 import { TableModule } from "primeng/table";
 import { CommonModule, NgClass } from "@angular/common";
@@ -14,12 +11,10 @@ import {
   FormsModule,
   ReactiveFormsModule,
 } from "@angular/forms";
-// import { v4 as uuidv4 } from "uuid";
 import { NgIf } from "@angular/common";
 import { FormTableGroup } from "../../../pages/invoice-editor-page/invoice-editor-page.component";
 import { calculateTotals } from "../../../shared/functions/calculate-totals";
 import { createTableFormGroup } from "../../../shared/functions/create-table-form-group";
-// import { TableUserInputs } from "../../../enums/invoice-document.enum";
 
 export interface FormTableValue {
   item: string | null;
@@ -46,26 +41,11 @@ export interface FormTableValue {
   templateUrl: "./invoice-table.component.html",
   styleUrls: ["./invoice-table.component.scss"],
 })
-export class InvoiceEditorTableComponent implements OnInit {
-  // @Input() invoiceTableRowData?: TableUserInputs[];
+export class InvoiceEditorTableComponent{
   @Input({ required: true }) editMode!: boolean;
   @Input({ required: true }) invoiceTableForm!: FormArray<FormGroup<FormTableGroup>>;
 
   public checkRowValidations: boolean = false;
-  // @Output() formChanges = new EventEmitter();
-
-  // public tableRows: TableUserInputs[] = [];
-
-  ngOnInit(): void {
-    // const savedTableRows = this.invoiceTableRowData;
-    // //If we have saved data for this invoice saved, we want to populate tableRows OnInit
-    // if ((savedTableRows) && savedTableRows.length > 0) {
-    //   savedTableRows.forEach((row) => {
-    //     //Push saved data from rows into our component table rows
-    //     this.tableRows.push(row);
-    //   });
-    // }
-  }
 
   public columns = [
     { field: "item", header: "Item", class: "w-30" },
@@ -97,52 +77,9 @@ export class InvoiceEditorTableComponent implements OnInit {
 
   }
 
-  //Updates calculation for saved invoicing table totals, and updates dom accordingly
-  // public updateRowCalculations(rowId: string): void {
-  //   const tableRow = document.getElementById(`${rowId}`);
-  //   const tableRowArrayObject = this.tableRows.find(
-  //     (obj) => obj.rowId === rowId
-  //   );
-
-  //   if (tableRowArrayObject && tableRow) {
-  //     //For the row with the matching ID, we will use these values to calculate our fields
-  //     const quantity = parseFloat(tableRowArrayObject.quantity as string);
-  //     const unitNetPrice = parseFloat(tableRowArrayObject.unitNetPrice as string);
-  //     const vatPercentage = parseFloat(tableRowArrayObject.vatPercentage as string);
-  //     const totalNet = quantity * unitNetPrice;
-  //     //Input values we want to malipulate
-  //     const totalNetHTML = tableRow?.querySelector('td input[name="totalNet"]') as HTMLInputElement;
-  //     const totalGrossHTML = tableRow?.querySelector('td input[name="totalGross"]') as HTMLInputElement;
-  //     //Returns calculations into cells as values
-  //     const totalNetCalculated = parseFloat((quantity * unitNetPrice).toFixed(2));
-  //     const totalGrossCalculated = parseFloat((totalNet + ((vatPercentage / 100) * totalNet)).toFixed(2));
-  //     totalNetHTML.value = totalNetCalculated ? totalNetCalculated.toString() : "";
-  //     totalGrossHTML.value = totalGrossCalculated
-  //       ? totalGrossCalculated.toString()
-  //       : totalNetCalculated
-  //       ? totalNetCalculated.toString()
-  //       : "";
-
-  //     tableRowArrayObject.totalNet = parseFloat(totalNetHTML.value).toFixed(2);
-  //     tableRowArrayObject.totalGross = parseFloat(totalGrossHTML.value).toFixed(2);
-
-  //     //We want to signal to the parent component that totals need to be re-calculated, and supply the tableRows with data
-  //     this.formChanges.emit(this.tableRows);
-  //   }
-  // }
-
   public deleteRowFromTable(tableRow: FormGroup<FormTableGroup>): void {
     const rowIndex = this.invoiceTableForm.controls.indexOf(tableRow);
 
     this.invoiceTableForm.removeAt(rowIndex);
   }
-
-  // private _addDeleteRowAnimation(id: string) {
-  //   const rowElement = document.getElementById(`${id}`);
-
-  //   if (rowElement) {
-  //     rowElement.classList.remove("appear-animation");
-  //     rowElement.classList.add("slide-out-animation");
-  //   }
-  // }
 }
